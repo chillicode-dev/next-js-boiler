682b799bc13c0f1e76e2ffca83978150956285c2
cutlet_kiev <ElinaRasskazova@gmail.com>
2020-03-05 18:10:48 +0300

    PrivacyPolicy: edit text style

--- sections/terms/TermsPage/TermsPage.js
+++ sections/terms/TermsPage/TermsPage.js
@@ -7,7 +7,7 @@ import style from './style.scss';
 const TermsPage = () => {
   return (
     <div className={style.TermsPage}>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Agreement between user and PullQuest Welcome to&nbsp;PullQuest.net. The PullQuest.net website (the
         &laquo;Site&raquo;) is&nbsp;comprised of&nbsp;various web pages operated by&nbsp;PullQuest LLC
         (&laquo;PullQuest&raquo;). PullQuest.net is&nbsp;offered to&nbsp;you conditioned on&nbsp;your acceptance without
@@ -15,21 +15,21 @@ const TermsPage = () => {
         of&nbsp;PullQuest.net constitutes your agreement to&nbsp;all such Terms. Please read these terms carefully, and
         keep a&nbsp;copy of&nbsp;them for your reference.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         PullQuest.net is&nbsp;a&nbsp;E-commerce Site Providing Website Engagement Software for Businesses
         as&nbsp;a&nbsp;service.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Privacy Your use of&nbsp;PullQuest.net is&nbsp;subject to&nbsp;PullQuest&rsquo;s Privacy Policy. Please review
         our Privacy Policy, which also governs the Site and informs users of&nbsp;our data collection practices.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Electronic Communications Visiting PullQuest.net or&nbsp;sending emails to&nbsp;PullQuest constitutes electronic
         communications. You consent to&nbsp;receive electronic communications and you agree that all agreements,
         notices, disclosures and other communications that we&nbsp;provide to&nbsp;you electronically, via email and
         on&nbsp;the Site, satisfy any legal requirement that such communications be&nbsp;in&nbsp;writing.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Your account&nbsp;If you use this site, you are responsible for maintaining the confidentiality of&nbsp;your
         account and password and for restricting access to&nbsp;your computer, and you agree to&nbsp;accept
         responsibility for all activities that occur under your account or&nbsp;password. You may not assign
@@ -38,12 +38,12 @@ const TermsPage = () => {
         or&nbsp;misappropriation of&nbsp;your account. PullQuest and its associates reserve the right to&nbsp;refuse
         or&nbsp;cancel service, terminate accounts, or&nbsp;remove or&nbsp;edit content in&nbsp;our sole discretion.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         PullQuest does not knowingly collect, either online or&nbsp;offline, personal information from persons under the
         age of&nbsp;thirteen. If&nbsp;you are under&nbsp;18, you may use PullQuest.net only with permission
         of&nbsp;a&nbsp;parent or&nbsp;guardian.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Links to&nbsp;third party sites/Third party services PullQuest.net may contain links to&nbsp;other websites
         (&laquo;Linked Sites&raquo;). The Linked Sites are not under the control of&nbsp;PullQuest and PullQuest
         is&nbsp;not responsible for the contents of&nbsp;any Linked Site, including without limitation any link
@@ -51,14 +51,14 @@ const TermsPage = () => {
         is&nbsp;providing these links to&nbsp;you only as&nbsp;a&nbsp;convenience, and the inclusion of&nbsp;any link
         does not imply endorsement by&nbsp;PullQuest of&nbsp;the site or&nbsp;any association with its operators.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Certain services made available via PullQuest.net are delivered by&nbsp;third party sites and organizations.
         By&nbsp;using any product, service or&nbsp;functionality originating from the PullQuest.net domain, you hereby
         acknowledge and consent that PullQuest may share such information and data with any third party with whom
         PullQuest has a&nbsp;contractual relationship to&nbsp;provide the requested product, service
         or&nbsp;functionality on&nbsp;behalf of&nbsp;PullQuest.net users and customers.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         No&nbsp;unlawful or&nbsp;prohibited use/Intellectual Property You are granted a&nbsp;non-exclusive,
         non-transferable, revocable license to&nbsp;access and use PullQuest.net strictly in&nbsp;accordance with these
         terms of&nbsp;use. As&nbsp;a&nbsp;condition of&nbsp;your use of&nbsp;the Site, you warrant to&nbsp;PullQuest
@@ -82,14 +82,14 @@ const TermsPage = () => {
         We&nbsp;do&nbsp;not grant you any licenses, express or&nbsp;implied, to&nbsp;the intellectual property
         of&nbsp;PullQuest or&nbsp;our licensors except as&nbsp;expressly authorized by&nbsp;these Terms.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         International Users The Service is&nbsp;controlled, operated and administered by&nbsp;PullQuest from our offices
         within the USA. If&nbsp;you access the Service from a&nbsp;location outside the USA, you are responsible for
         compliance with all local laws. You agree that you will not use the PullQuest Content accessed through
         PullQuest.net in&nbsp;any country or&nbsp;in&nbsp;any manner prohibited by&nbsp;any applicable laws,
         restrictions or&nbsp;regulations.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Indemnification You agree to&nbsp;indemnify, defend and hold harmless PullQuest, its officers, directors,
         employees, agents and third parties, for any losses, costs, liabilities and expenses (including reasonable
         attorneys&rsquo; fees) relating to&nbsp;or&nbsp;arising out of&nbsp;your use of&nbsp;or&nbsp;inability
@@ -98,17 +98,17 @@ const TermsPage = () => {
         violation of&nbsp;any applicable laws, rules or&nbsp;regulations. PullQuest reserves the right, at&nbsp;its own
         cost, to&nbsp;assume the exclusive defense and control of&nbsp;any matter
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         otherwise subject to indemnification by you, in which event you will fully cooperate with PullQuest in asserting
         any available defenses.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml uppercase>
+      <Typography variant="paragraph" hasMargin uppercase>
         Liability disclaimer THE INFORMATION, SOFTWARE, PRODUCTS, AND SERVICES INCLUDED IN&nbsp;OR&nbsp;AVAILABLE
         THROUGH THE SITE MAY INCLUDE INACCURACIES OR&nbsp;TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY ADDED
         TO&nbsp;THE INFORMATION HEREIN. PullQuest LLC&nbsp;AND/OR ITS SUPPLIERS MAY MAKE IMPROVEMENTS AND/OR CHANGES
         IN&nbsp;THE SITE AT&nbsp;ANY TIME.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml uppercase>
+      <Typography variant="paragraph" hasMargin uppercase>
         PullQuest LLC&nbsp;AND/OR ITS SUPPLIERS MAKE NO&nbsp;REPRESENTATIONS ABOUT THE SUITABILITY, RELIABILITY,
         AVAILABILITY, TIMELINESS, AND ACCURACY OF&nbsp;THE INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED
         GRAPHICS CONTAINED ON&nbsp;THE SITE FOR ANY PURPOSE. TO&nbsp;THE MAXIMUM EXTENT PERMITTED BY&nbsp;APPLICABLE
@@ -118,7 +118,7 @@ const TermsPage = () => {
         SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS, INCLUDING ALL&nbsp;IMPLIED WARRANTIES OR&nbsp;CONDITIONS
         OF&nbsp;MERCHANTABILITY, FITNESS FOR A&nbsp;PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml uppercase>
+      <Typography variant="paragraph" hasMargin uppercase>
         TO&nbsp;THE MAXIMUM EXTENT PERMITTED BY&nbsp;APPLICABLE LAW, IN&nbsp;NO&nbsp;EVENT SHALL PullQuest
         LLC&nbsp;AND/OR ITS SUPPLIERS BE&nbsp;LIABLE FOR ANY DIRECT, INDIRECT, PUNITIVE, INCIDENTAL, SPECIAL,
         CONSEQUENTIAL DAMAGES OR&nbsp;ANY DAMAGES WHATSOEVER INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS
@@ -133,7 +133,7 @@ const TermsPage = () => {
         IF&nbsp;YOU ARE DISSATISFIED WITH ANY PORTION OF&nbsp;THE SITE, OR&nbsp;WITH ANY OF&nbsp;THESE TERMS
         OF&nbsp;USE, YOUR SOLE AND EXCLUSIVE REMEDY IS&nbsp;TO&nbsp;DISCONTINUE USING THE SITE.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Termination/access restriction PullQuest reserves the right, in&nbsp;its sole discretion, to&nbsp;terminate your
         access to&nbsp;the Site and the related services or&nbsp;any portion thereof at&nbsp;any time, without notice.
         To&nbsp;the maximum extent permitted by&nbsp;law, this agreement is&nbsp;governed by&nbsp;the laws of&nbsp;the
@@ -142,7 +142,7 @@ const TermsPage = () => {
         of&nbsp;the Site is&nbsp;unauthorized in&nbsp;any jurisdiction that does not give effect to&nbsp;all provisions
         of&nbsp;these Terms, including, without limitation, this section.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         You agree that no&nbsp;joint venture, partnership, employment, or&nbsp;agency relationship exists between you
         and PullQuest as&nbsp;a&nbsp;result of&nbsp;this agreement or&nbsp;use of&nbsp;the Site. PullQuest&rsquo;s
         performance of&nbsp;this agreement is&nbsp;subject to&nbsp;existing laws and legal process, and nothing
@@ -155,7 +155,7 @@ const TermsPage = () => {
         by&nbsp;a&nbsp;valid, enforceable provision that most closely matches the intent of&nbsp;the original provision
         and the remainder of&nbsp;the agreement shall continue in&nbsp;effect.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Unless otherwise specified herein, this agreement constitutes the entire agreement between the user and
         PullQuest with respect to&nbsp;the Site and it&nbsp;supersedes all prior or&nbsp;contemporaneous communications
         and proposals, whether electronic, oral or&nbsp;written, between the user and PullQuest with respect to&nbsp;the
@@ -165,25 +165,25 @@ const TermsPage = () => {
         documents and records originally generated and maintained in&nbsp;printed form. It&nbsp;is&nbsp;the express wish
         to&nbsp;the parties that this agreement and all related documents be&nbsp;written in&nbsp;English.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Changes to&nbsp;Terms PullQuest reserves the right, in&nbsp;its sole discretion, to&nbsp;change the Terms under
         which PullQuest.net is&nbsp;offered. The most current version of&nbsp;the Terms will supersede all previous
         versions. PullQuest encourages you to&nbsp;periodically review the Terms to&nbsp;stay informed of&nbsp;our
         updates.
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         Contact Us PullQuest welcomes your questions or comments regarding the Terms:
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography variant="paragraph" hasMargin>
         PullQuest LLC 446&nbsp;S La&nbsp;Brea Ave 90036 Los Angeles, CA
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography className={style.text} variant="paragraph" hasMargin>
         Email Address: info@PullQuest.net
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography className={style.text} variant="paragraph" hasMargin>
         Telephone number: 310-402-8110
       </Typography>
-      <Typography className={style.text} variant="paragraph" hasHtml>
+      <Typography className={style.text} variant="paragraph" hasMargin>
         Effective as of June 01, 2018
       </Typography>
     </div>
