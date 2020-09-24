639dc4047687787282a5abb797932428fecda620
adlite <adlite@yandex.ru>
2020-09-24 12:14:14 +0300

    .stylelintrc.js: add new rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,5 +8,29 @@ module.exports = {
   plugins: ['stylelint-scss', 'stylelint-prettier'],
   rules: {
     'prettier/prettier': true,
+    'scss/at-mixin-argumentless-call-parentheses': 'always',
+    'scss/at-mixin-parentheses-space-before': 'never',
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'selector-combinator-space-after': 'always',
+    'selector-pseudo-class-case': 'lower',
+    'selector-pseudo-element-case': 'lower',
+    'selector-type-case': 'lower',
+    'selector-descendant-combinator-no-non-space': true,
+    'number-leading-zero': 'always',
+    'declaration-no-important': true,
+    'number-no-trailing-zeros': true,
+    'length-zero-no-unit': true,
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['after-single-line-comment', 'first-nested'],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
   },
 };
