68636d0eeb5f6b0f870139956ac6fa36de239267
Adlite <adlite@yandex.ru>
2020-09-27 00:19:36 +0300

    Add utils, add Api service and replace 'classes' dir to 'services'

---
+++ utils/enums.js
@@ -0,0 +1,16 @@
+/**
+ * Enumerable structures
+ */
+
+/**
+ * @typedef HttpMethod
+ * @type {{DELETE: string, POST: string, GET: string, PUT: string, PATCH: string}}
+ */
+export const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+  HEAD: 'HEAD',
+};
